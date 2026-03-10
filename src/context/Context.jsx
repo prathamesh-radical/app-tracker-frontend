c480fac0e3f891af6936f31a8ca4d768cb555789
prathamesh-radical <prathmesh.n@radicalglobal.net>
2026-03-10 17:56:05 +0530

    functionality improved

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,17 +1,7 @@
 import { useState } from "react";
-import useFetch from "../hooks/useFetch";
 import { MyContext } from "./context";
 
 export const MyContextProvider = ({ children }) => {
-    const { data: debtorsData, loading: debtorsLoading, error: debtorsError } = useFetch('/api/debtors');
-    const { data: mechanicData, loading: mechanicLoading, error: mechanicError } = useFetch('/api/mechanic');
-    const { data: mechanicUserData, loading: mechanicUserDataLoading, error: mechanicUserDataError } = useFetch('/api/mechanic-data');
-    const { data: mechanicServicesData, loading: mechanicServicesDataLoading, error: mechanicServicesDataError } = useFetch('/api/mechanic-services-data');
-    const { data: moneycollectData, loading: moneycollectLoading, error: moneycollectError } = useFetch('/api/money-collect');
-    const { data: moneycollectUserData, loading: moneycollectUserDataLoading, error: moneycollectUserDataError } = useFetch('/api/money-collect-data');
-    const { data: guestEntriesData, loading: guestEntriesLoading, error: guestEntriesError } = useFetch('/api/guest-entries');
-    const { data: guestEntriesUserData, loading: guestEntriesUserDataLoading, error: guestEntriesUserDataError } = useFetch('/api/guest-entries-data');
-    const loader = debtorsLoading || mechanicLoading || mechanicUserDataLoading || mechanicServicesDataLoading || moneycollectLoading || guestEntriesLoading || moneycollectUserDataLoading || guestEntriesUserDataLoading;
     const [registerFormData, setRegisterFormData] = useState({
         email: "",
         password: "",
@@ -33,7 +23,7 @@ export const MyContextProvider = ({ children }) => {
     }
 
     const values = {
-        debtorsData, debtorsLoading, debtorsError, mechanicData, mechanicLoading, mechanicError, mechanicUserData, mechanicUserDataLoading, mechanicUserDataError, mechanicServicesData, mechanicServicesDataLoading, mechanicServicesDataError, moneycollectData, moneycollectLoading, moneycollectError, moneycollectUserData, moneycollectUserDataLoading, moneycollectUserDataError, guestEntriesData, guestEntriesLoading, guestEntriesError, guestEntriesUserData, guestEntriesUserDataLoading, guestEntriesUserDataError, loader, registerFormData, handleChange, loading, setLoading, loginFormData, token, setToken, isAuthenticated, setIsAuthenticated, handleNull
+        registerFormData, handleChange, loading, setLoading, loginFormData, token, setToken, isAuthenticated, setIsAuthenticated, handleNull
     };
 
     return (
